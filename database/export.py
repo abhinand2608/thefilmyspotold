f947a68af8d30196ab3b482da4248084dceffe29
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2023-02-04 06:51:54 +0530

    Update export.py

--- database/export.py
+++ database/export.py
@@ -1,24 +1,25 @@
 import pymongo
 import telegram
 import os
+from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME
+from utils import get_settings, save_group_settings
 
 # Connection string to connect to the MongoDB Atlas cluster
-uri = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/test?retryWrites=true&w=majority"
-
+uri = DATABASE_URI
 # Connect to the cluster
 client = pymongo.MongoClient(uri)
 
 # Get the database you want to export data from
-db = client["mydatabase"]
+db = client[DATABASE_NAME]
 
 # Get the collection you want to export data from
-collection = db["mycollection"]
+collection = db[COLLECTION_NAME]
 
 # Retrieve all files from the collection
 files = collection.find({})
 
 # Initialize the Telegram Bot API client
-bot = telegram.Bot(token='YOUR_TELEGRAM_BOT_TOKEN')
+bot = telegram.Bot(token='BOT_TOKEN')
 
 # Loop through the files and export each one to the Telegram channel
 for file in files:
@@ -35,11 +36,11 @@ for file in files:
 
     try:
         if file_type in ['mp4', 'avi', 'mkv']:
-            bot.send_video(chat_id='-100YOUR_CHANNEL_ID', video=open(file['path'], 'rb'))
+            bot.send_video(chat_id='-1001694860471', video=open(file['path'], 'rb'))
             print(f"Video {file['path']} exported successfully.")
         else:
             with open(file['path'], 'rb') as f:
-                bot.send_document(chat_id='-100YOUR_CHANNEL_ID', document=f)
+                bot.send_document(chat_id='-1001694860471', document=f)
                 print(f"File {file['path']} exported successfully.")
     except Exception as e:
         print(f"Failed to export file {file['path']}: {e}")
